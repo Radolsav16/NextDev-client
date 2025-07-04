5bceff59baef0de5142554f0a1d5fbf251b6e3d4
Radoslav <todorovradoslav6@gmail.com>
2025-07-04 15:53:00 +0300

    make loader and make fisrt route to register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import Hero from "./components/hero/Hero"
-import Statistics from "./components/statistics/Statistics"
+import {lazy, Suspense } from "react";
+import { Routes,Route } from "react-router";
+import { delayer } from "./utils/delayer";
+
+
+import Loader from "./utilities/loader/Loader";
 import Footer from "./components/footer/Footer";
 
-import Subscribe from "./components/subscribe/Subscribe"
 import "./styles.css"
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -10,15 +13,33 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 
+const MS = 2600;
+
+
+const Home = lazy(()=>
+  delayer(()=>import('./pages/Home'),MS)
+)
+const SignIn = lazy(()=>
+delayer(()=>import('./pages/SignIn'),MS)
+)
+
 function App() {
  
 
   return (
     <>
-      <Hero />
-      <Subscribe />
-      <Statistics />
-      <Footer />
+   
+    <Suspense fallback={<Loader/>}>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/next-dev/sign-in" element={<SignIn />}/>
+
+    </Routes>
+    </Suspense>
+     
+
+    <Footer />
+
     </>
   )
 }
